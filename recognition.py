a1d3deed03e5a098e779f7bd92bb0b34dbbb7ff4
Quentin Durand <jayecon96@gmail.com>
2019-07-07 13:39:32 +0200

    some experimentation on the recognition

--- recognition.py
+++ recognition.py
@@ -84,15 +84,17 @@ while True:
             name = "Unknown"
 
             # # If a match was found in known_face_encodings, just use the first one.
-            # if True in matches:
-            #     first_match_index = matches.index(True)
-            #     name = known_face_names[first_match_index]
+            if True in matches:
+                 first_match_index = matches.index(True)
+                 name = known_face_names[first_match_index]
+                 os.system(
+                     "echo \'OK: Identification succees: " + name + " sucessfully identified\\n\' > identification.txt")
 
             '''
                     I still need to do tests to compare the two methods and find the best
             '''
             # Or instead, use the known face with the smallest distance to the new face
-            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            '''face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
             best_match_index = np.argmin(face_distances)
             if matches[best_match_index]:
                 name = known_face_names[best_match_index]
@@ -100,7 +102,7 @@ while True:
 
             else:
                 os.system("echo 'KO: No known subject identified\n' > identification.txt")
-            face_names.append(name)
+            face_names.append(name)'''
 
     process_this_frame = not process_this_frame
 
